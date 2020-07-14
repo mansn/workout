48e4447562d1b37d422ec199011f3cb77e8db934
mansn <4518977+mansn@users.noreply.github.com>
2020-07-15 01:19:11 +0200

    feat: start navbar with temporary styling

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import { Router, Link } from '@reach/router'
 import { hot } from 'react-hot-loader'
 import './App.css'
 import Workout from './components/Workout'
-import CreateExercise from './components/CreateExercise'
+import CreateExercise from './components/CreateExercise/CreateExercise'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header>
-          <h1> Hello, Workout! 🏋️‍♂️</h1>
-          <Link to="/create-exercise">Create Exercise</Link>
+          <Link to="/">
+            <h1> Hello, Workout! 🏋️‍♂️</h1>
+          </Link>
+          <nav>
+            <Link to="/create-exercise">Create Exercise</Link>
+          </nav>
         </header>
         <div className="container">
           <Router>
