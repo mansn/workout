9484b6b57338efc2dcd0b2145f6256c4e30d033e
mansn <4518977+mansn@users.noreply.github.com>
2020-08-20 22:25:03 +0200

    feat(user-auth): move LoginButton component and add css module

--- src/components/LoginButton/LoginButton.js
+++ src/components/LoginButton/LoginButton.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
+import styles from './LoginButton.module.css'
 
 const LoginButton = () => {
   const { loginWithRedirect, isAuthenticated } = useAuth0()
 
-  return !isAuthenticated && <button onClick={() => loginWithRedirect()}>Log In</button>
+  return !isAuthenticated && <button className={styles.loginBtn} onClick={() => loginWithRedirect()}>Log In</button>
 }
 
 export default LoginButton
