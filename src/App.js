8f97ef46d1743e5ad603cf14362b21942b50db30
mansn <4518977+mansn@users.noreply.github.com>
2020-08-20 22:24:01 +0200

    feat(user-auth): don't render workout for non-authenticated users

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Router, Link } from '@reach/router'
-import LoginButton from './components/LoginButton'
+import LoginButton from './components/LoginButton/LoginButton'
 import LogoutButton from './components/LogoutButton'
 import { hot } from 'react-hot-loader'
 import './App.css'
 import Workout from './components/Workout'
+import { useAuth0 } from '@auth0/auth0-react'
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header>
-          <div className="inner-header">
-            <Link to="/">
-              <h1> Hello, Workout! 🏋️‍♂️</h1>
-            </Link>
+function App() {
+  const { user, isAuthenticated } = useAuth0()
+
+  return (
+    <div className="App">
+      <header>
+        <div className="inner-header">
+          <Link to="/">
+            <h1>Hello, Workout! 🏋️‍♂️</h1>
+          </Link>
+          <div className="auth">
             <LoginButton />
             <LogoutButton />
           </div>
-        </header>
+        </div>
+      </header>
+      {isAuthenticated && (
         <div className="container">
           <Router>
-            <Workout path="/" />
+            <Workout user={user} path="/" />
           </Router>
         </div>
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
 export default hot(module)(App)
