12cc461e646f3c7561b32d6d62b638cd7fd6dad6
mansn <4518977+mansn@users.noreply.github.com>
2020-08-22 23:20:06 +0200

    fix: show exercise save button for authorized user

--- src/components/Exercise.js
+++ src/components/Exercise.js
@@ -2,7 +2,6 @@ import React, { useState } from 'react'
 import axios from 'axios'
 import Input from './Input/Input'
 import SaveButton from './SaveButton'
-import { useAuth0 } from '@auth0/auth0-react'
 
 const Exercise = ({
   title,
@@ -15,15 +14,8 @@ const Exercise = ({
   workoutId,
   setWorkoutData
 }) => {
-  // TODO: Temporary solution to use the isAuthenticated hook here
-  // should try and move decisions on this higher up the hierarchy
-  const { isAuthenticated } = useAuth0()
   const [modified, setModified] = useState(false)
   const [saveStatus, setSaveStatus] = useState('save')
-  const shouldRenderSaveBtn = () => {
-    if (!isAuthenticated && modified) return false
-    return false
-  }
   const handleSave = event => {
     setSaveStatus('saving')
     event.preventDefault()
@@ -102,7 +94,7 @@ const Exercise = ({
           <h3>{`(${sets}x ${recommendedReps.min}-${recommendedReps.max})`}</h3>
         </div>
         {inputFields}
-        {shouldRenderSaveBtn() && <SaveButton handleSave={handleSave} saveStatus={saveStatus} />}
+        {modified && <SaveButton handleSave={handleSave} saveStatus={saveStatus} />}
       </li>
     </>
   )
--- src/components/SaveButton.js
+++ src/components/SaveButton.js
@@ -1,7 +1,13 @@
 import React from 'react'
+import { useAuth0 } from '@auth0/auth0-react'
 
-// TODO: Fix ugly ternary
 const SaveButton = ({ handleSave, saveStatus }) => {
+  // TODO: Fix ugly ternary, and remove temporary solution to use the isAuthenticated hook here
+  // should try and move decisions on this higher up the component hierarchy
+  const { isAuthenticated } = useAuth0()
+
+  if (!isAuthenticated) return null
+
   return (
     <button className={['save-btn', saveStatus].join(' ')} onClick={handleSave}>
       {saveStatus === 'saving' ? (
